fa17d8fdccfe4c9d540533039f135401cd24936f
bndatum <123423888+bndatum@users.noreply.github.com>
2024-02-22 23:29:14 -0600

    Update ProductosServices.java

--- src/main/java/com/datumredsoft/shop/services/ProductosServices.java
+++ src/main/java/com/datumredsoft/shop/services/ProductosServices.java
@@ -2,6 +2,7 @@ package com.datumredsoft.shop.services;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Iterator;
@@ -18,13 +19,59 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service("ProductosServices")
 public class ProductosServices implements ProductosInterface {
 	
-
 	@Override
+	public RespuestaModelo getAllProductos() {
+        RespuestaModelo response = new RespuestaModelo();
+        List<ProductosModelo> listaProductos = new ArrayList<>();
+
+        try {
+            // Obtén el ClassLoader para acceder a los recursos
+            ClassLoader classLoader = getClass().getClassLoader();
+
+            // Obtén la URL del directorio de recursos
+            URL resource = classLoader.getResource("img");
+            if (resource == null) {
+                throw new IllegalArgumentException("Directorio de recursos no encontrado");
+            }
+
+            // Convierte la URL a un objeto File
+            File carpeta = new File(resource.getFile());
+
+            ObjectMapper mapper = new ObjectMapper();
+            System.out.println("La lista es");
+            System.out.println(mapper.writeValueAsString(carpeta.list()));
+
+            // Ciclo para Saturación RAM y CPU
+            for (int i = 0; i < 2; i++) {
+                for (String nombreIma : carpeta.list()) {
+                    byte[] fileContent;
+                    ProductosModelo product = new ProductosModelo();
+
+                    fileContent = FileUtils.readFileToByteArray(new File(carpeta, nombreIma));
+                    String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+                    product.setNombreProducto(nombreIma);
+                    product.setImagen(encodedString);
+                    if (i == 0)
+                        listaProductos.add(product);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        response.setProductoRespuesta(listaProductos);
+
+        return response;
+    }
+	
+	/*@Override
 	public RespuestaModelo getAllProductos() {
 		RespuestaModelo response = new RespuestaModelo();
 		List<ProductosModelo> listaProductos = new ArrayList<>();
 		
 		try {
+			
 			File carpeta = new File("src/main/resources/img");
 			ObjectMapper mapper = new ObjectMapper();
 			System.out.println("La lista es");
@@ -52,9 +99,65 @@ public class ProductosServices implements ProductosInterface {
 		response.setProductoRespuesta(listaProductos);
 		
 		return response;
-	}
+	}*/
 
 	@Override
+	public RespuestaModelo getAllProductoByCantidad(int limite) {
+        RespuestaModelo response = new RespuestaModelo();
+        List<ProductosModelo> listaProductos = new ArrayList<>();
+        List<ProductosModelo> listaProductosResponse = new ArrayList<>();
+
+        try {
+            if (limite > 30)
+                limite = 30;
+
+            // Obtén el ClassLoader para acceder a los recursos
+            ClassLoader classLoader = getClass().getClassLoader();
+
+            // Obtén la URL del directorio de recursos
+            URL resource = classLoader.getResource("img");
+            if (resource == null) {
+                throw new IllegalArgumentException("Directorio de recursos no encontrado");
+            }
+
+            // Convierte la URL a un objeto File
+            File carpeta = new File(resource.getFile());
+
+            ObjectMapper mapper = new ObjectMapper();
+            System.out.println("La lista es");
+            System.out.println(mapper.writeValueAsString(carpeta.list()));
+
+            // Ciclo para Saturación RAM y CPU
+            for (int i = 0; i < 5; i++) {
+                for (String nombreIma : carpeta.list()) {
+                    byte[] fileContent;
+                    ProductosModelo product = new ProductosModelo();
+                    fileContent = FileUtils.readFileToByteArray(new File(carpeta, nombreIma));
+                    String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+                    product.setNombreProducto(nombreIma);
+                    product.setImagen(encodedString);
+                    if (i == 0)
+                        listaProductos.add(product);
+                }
+            }
+
+            Random rand = new Random();
+            for (int i = 0; i < limite; i++) {
+                int randomIndex = rand.nextInt(listaProductos.size());
+                ProductosModelo randomElement = listaProductos.get(randomIndex);
+                listaProductosResponse.add(randomElement);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        response.setProductoRespuesta(listaProductosResponse);
+
+        return response;
+    }
+	
+	/*@Override
 	public RespuestaModelo getAllProductoByCantidad(int limite) {
 		RespuestaModelo response = new RespuestaModelo();
 		List<ProductosModelo> listaProductos = new ArrayList<>();
@@ -99,6 +202,6 @@ public class ProductosServices implements ProductosInterface {
 		
 		return response;
 		
-	}
+	}*/
 
 }
